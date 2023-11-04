6ec85509b52e9c2bb6f921ca4ef0ec0f00b3a7e6
Sodara Sou <sodarasou168@gmail.com>
2023-11-04 20:45:49 +0700

    Add search functionality and password visibility toggle, and update SVG icons

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import cartSvg from "../assets/svg/cart-plus-solid.svg";
 import accountSvg from "../assets/svg/user-regular.svg";
+import SearhSvg from "../assets/svg/magnifying-glass-solid.svg";
 import Logo from "../assets/img/Logo 240 x 56.png";
 
 function Header() {
@@ -12,12 +13,15 @@ function Header() {
             <img src={Logo} alt="Logo" />
           </Link>
           <div className="flex items-center text-lg">
-            <div className="hidden md:block">
+            <div className="hidden md:flex relative">
               <input
                 type="text"
-                className="w-[347px] h-[38px] px-4 rounded-lg bg-[#D9D9D9] mr-10"
+                className="w-[347px] h-[38px] bg-[#EAECF6] rounded-xl px-4 py-2 border focus:outline-none focus:ring focus:border-[#5E17EB] mr-10"
                 placeholder="Search Product"
               />
+              <button className="svg-size absolute top-[10px] right-14">
+                <img src={SearhSvg} alt="Search" />
+              </button>
             </div>
             <ul className="flex gap-4 md:gap-8">
               <li className="flex items-center">
@@ -42,12 +46,15 @@ function Header() {
             </ul>
           </div>
         </div>
-        <div className="block md:hidden">
+        <div className="flex md:hidden relative">
           <input
             type="text"
-            className="w-full h-[38px] px-4 rounded-lg bg-[#D9D9D9]"
+            className="w-full h-[38px] bg-[#EAECF6] rounded-xl px-4 py-2 border focus:outline-none focus:ring focus:border-[#5E17EB]"
             placeholder="Search Product"
           />
+          <button className="svg-size absolute top-[10px] right-4">
+            <img src={SearhSvg} alt="Search" />
+          </button>
         </div>
       </div>
     </nav>
--- src/ui/shared/Input.jsx
+++ src/ui/shared/Input.jsx
@@ -1,17 +1,42 @@
+import { useState } from "react";
+import ShowPassword from "../../assets/svg/eye-solid.svg";
+import HidePassword from "../../assets/svg/eye-slash-solid.svg";
+
 function Input({ title, type, placeholder, id, onChange, value }) {
+  const isPasswordInput = type === "password";
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
   return (
     <div className="flex flex-col gap-2">
       <label htmlFor={id} className="text-lg font-semibold">
         {title}
       </label>
-      <input
-        type={type}
-        placeholder={placeholder}
-        id={id}
-        onChange={onChange}
-        value={value}
-        className="rounded-xl w-full px-4 py-2 border"
-      />
+      <div className="relative flex">
+        <input
+          type={isPasswordInput && showPassword ? "text" : type}
+          placeholder={placeholder}
+          id={id}
+          onChange={onChange}
+          value={value}
+          className="rounded-xl w-full px-4 py-2 border focus:outline-none focus:ring focus:border-[#5E17EB]"
+        />
+        {isPasswordInput && (
+          <button
+            type="button"
+            onClick={togglePasswordVisibility}
+            className="svg-size absolute top-[12px] right-3"
+          >
+            {showPassword ? (
+              <img src={HidePassword} alt="Hide Password" />
+            ) : (
+              <img src={ShowPassword} alt="Show Password" />
+            )}
+          </button>
+        )}
+      </div>
     </div>
   );
 }
