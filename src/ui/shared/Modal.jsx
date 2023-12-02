fba0ed894a135820626956cb637d7ae31ae0e1a7
Sodara Sou <sodarasou168@gmail.com>
2023-12-02 23:05:46 +0700

    Add Modal component

---
+++ src/ui/shared/Modal.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useRef } from "react";
+import { motion } from "framer-motion";
+
+function Modal({ children, handleModal }) {
+  const modalRef = useRef();
+  useEffect(() => {
+    const handler = (e) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(e.target) &&
+        e.target !== document.body
+      ) {
+        handleModal(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  }, []);
+  return (
+    <div className="p-4 fixed z-40 inset-0 bg-black bg-opacity-40 flex justify-center items-center">
+      <motion.div
+        key="modal"
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.8 }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+        ref={modalRef}
+        className="p-4 md:p-10 bg-[#EAECF6] rounded-2xl overflow-y-auto"
+      >
+        {children}
+      </motion.div>
+    </div>
+  );
+}
+
+export default Modal;
