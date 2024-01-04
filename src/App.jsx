5cca09f356b115b838ac0e2c862a5eeb0660a136
Sodara Sou <sodarasou168@gmail.com>
2024-01-04 17:04:22 +0700

    Remove unused import statement

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ import SignIn from "./pages/user/auth/SignIn";
 import SignUp from "./pages/user/auth/SignUp";
 import Order from "./pages/admin/order/Order";
 import ProductAdmin from "./pages/admin/product/ProductAdmin";
-import User from "./pages/admin/user/User";
 import Dashboard from "./pages/admin/Dashboard/Dashboard";
 import NotFoundPage from "./ui/NotFoundPage";
 import Spinner from "./ui/Spinner";
